752d3d61cc423e1486b182e0a7ec65392211f4cb
silvak <silvak.jeg@gmail.com>
2024-02-29 09:58:56 -0400

    feact(paymentDetails): add form payment details

--- src/screens/payment.jsx
+++ src/screens/payment.jsx
@@ -1,14 +1,147 @@
+import { useState } from "react";
 import React from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-function PaymentDetails() {
-  return <div>payment details</div>;
+// Hook
+function useForm(initialState) {
+  const [formState, setFormState] = useState(initialState);
+
+  const handleInputChange = (fieldName, value) => {
+    setFormState({
+      ...formState,
+      [fieldName]: value,
+    });
+  };
+
+  return [formState, handleInputChange];
 }
 
-import { productsData, totalProducts, category } from "@/mock/productData";
+function PaymentDetails() {
+  const [formState, handleInputChange] = useForm({
+    nombre: "",
+    apellido: "",
+    direccion1: "",
+    direccion2: "",
+    pais: "",
+    ciudad: "",
+    telefono: "",
+    correo: "",
+  });
+
+  const handleSubmit = () => {
+    console.log(formState);
+  };
+
+  return (
+    <Grid container columns={{ xs: 1, lg: 2 }} sx={{ width: "100%" }}>
+      {/* Column 1 */}
+      <Grid item sx={1}>
+        <Stack sx={{ float: "left", marginRight: "20px" }}>
+          <TextField
+            label="Nombre"
+            style={{
+              height: "60px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
+            value={formState.nombre}
+            onChange={(e) => handleInputChange("nombre", e.target.value)}
+          />
+          <TextField
+            label="Apellido"
+            style={{
+              height: "60px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
+            value={formState.apellido}
+            onChange={(e) => handleInputChange("apellido", e.target.value)}
+          />
+          <TextField
+            label="Dirección 1"
+            style={{
+              height: "60px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
+            value={formState.direccion1}
+            onChange={(e) => handleInputChange("direccion1", e.target.value)}
+          />
+          <TextField
+            label="Dirección 2"
+            style={{
+              height: "60px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
+            value={formState.direccion2}
+            onChange={(e) => handleInputChange("direccion2", e.target.value)}
+          />
+        </Stack>
+      </Grid>
+
+      {/* Columna 2 */}
+      <Grid item xs={1}>
+        <Stack sx={{ float: "left" }}>
+          <TextField
+            label="País"
+            style={{
+              height: "60px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
+            value={formState.pais}
+            onChange={(e) => handleInputChange("pais", e.target.value)}
+          />
+          <TextField
+            label="Ciudad"
+            style={{
+              height: "60px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
+            value={formState.ciudad}
+            onChange={(e) => handleInputChange("ciudad", e.target.value)}
+          />
+          <TextField
+            label="Número de Teléfono"
+            style={{
+              height: "60px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
+            value={formState.telefono}
+            onChange={(e) => handleInputChange("telefono", e.target.value)}
+          />
+          <TextField
+            label="Correo"
+            style={{
+              height: "60px",
+              border: "1px solid #ccc",
+              marginBottom: "10px",
+            }}
+            value={formState.correo}
+            onChange={(e) => handleInputChange("correo", e.target.value)}
+          />
+        </Stack>
+      </Grid>
+
+      {/* Button */}
+      <Grid item xs={1} lg={2}>
+        <Button sx={{ marginTop: "10px" }} onClick={handleSubmit}>
+          Enviar
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+import { productsData } from "@/mock/productData";
 
 const totalMock = [
   {
@@ -146,8 +279,20 @@ function ShoppingCartList() {
 
 function Payment() {
   return (
-    <div style={{ background: "white" }}>
-      <Grid container spacing={0} columns={{ xs: 1, lg: 2 }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "24px",
+        marginBottom: "80px",
+      }}
+    >
+      <Grid
+        container
+        spacing={0}
+        columns={{ xs: 1, lg: 2 }}
+        sx={{ maxWidth: "1300px" }}
+      >
         <Grid item xs={1}>
           <Typography
             variant="body1"
