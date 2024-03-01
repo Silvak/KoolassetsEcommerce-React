8897e1dce00e927240e769b91b88083a8b940453
francorinque <francoagustinrinquecorreo@gmail.com>
2024-03-01 13:01:32 -0300

    fix(modal): change state modal

--- src/stores/Actions/modal/storeModal.jsx
+++ src/stores/Actions/modal/storeModal.jsx
@@ -1,5 +1,5 @@
 export const storeModal = (set) => ({
-  stateModal: true,
-  contentModal: <div>Lorem ipsum dolor sit amet</div>,
+  stateModal: false,
+  contentModal: null,
   setModal: (open, content) => set({ stateModal: open, contentModal: content }),
 });
