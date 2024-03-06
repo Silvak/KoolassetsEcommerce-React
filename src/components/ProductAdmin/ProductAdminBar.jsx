c3aa8267cec6a9a97cc5aab6f34dfd076a1a55a2
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-03-06 09:45:42 -0300

    feat(product): implement fully responsive product filtering functionality

--- src/components/ProductAdmin/ProductAdminBar.jsx
+++ src/components/ProductAdmin/ProductAdminBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Button, MenuItem, Select, TextField, Typography, ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
+import { Button, Grid, MenuItem, Select, TextField, createTheme, useMediaQuery, ThemeProvider } from '@mui/material';
+
 
 const theme = createTheme({
     components: {
@@ -14,9 +15,6 @@ const theme = createTheme({
         },
         MuiSelect: {
             styleOverrides: {
-                root: {
-                    minWidth: 200,
-                },
                 icon: {
                     color: '#757575',
                 },
@@ -29,7 +27,9 @@ function ProductAdminBar() {
     const [orderBy, setOrderBy] = useState('');
     const [filterByValue, setFilterByValue] = useState('');
     const [filterByStatus, setFilterByStatus] = useState('');
-    const matches = useMediaQuery('(max-width:600px)'); 
+
+    const matches = useMediaQuery('(min-width:900px) and (max-width:1200px)'); 
+
 
     const handleOrderByChange = (event) => {
         setOrderBy(event.target.value);
@@ -47,63 +47,69 @@ function ProductAdminBar() {
     };
 
     return (
-        <>
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", height: 50 }}>
-                <div style={{ display: "flex" }}>
-                    <ThemeProvider theme={theme}>
-                        <TextField
-                            size='small'
-                            placeholder="#ID"
-                            style={{ marginRight: 10, width: 100 }}
-                        />
-                        <Select
-                            value={orderBy}
-                            size='small'
-                            onChange={handleOrderByChange}
-                            style={{ minWidth: 200, marginRight: 10, color: "#757575" }}
-                            displayEmpty
-                        >
-                            <MenuItem value="" style={{ display: 'none' }}>
-                                Fecha
-                            </MenuItem>
-                            <MenuItem value="Ascendente">Ascendente</MenuItem>
-                            <MenuItem value="Descendente">Descendente</MenuItem>
-                        </Select>
-                        <Select
-                            size='small'
-                            value={filterByValue}
-                            onChange={handleFilterByValueChange}
-                            style={{ minWidth: 200, marginRight: 10, color: "#757575" }}
-                            displayEmpty
-                        >
-                            <MenuItem value="" style={{ display: 'none' }}>
-                                Valor
-                            </MenuItem>
-                            <MenuItem value="$100-$1000">$100 a $1000</MenuItem>
-                            <MenuItem value="$1000-$10000">$1k a $10k</MenuItem>
-                            <MenuItem value="$10000-$100000">$10k a $100k</MenuItem>
-                        </Select>
-                        <Select
-                            size='small'
-                            value={filterByStatus}
-                            onChange={handleFilterByStatusChange}
-                            style={{ minWidth: 200, color: "#757575" }}
-                            displayEmpty
-                        >
-                            <MenuItem value="" style={{ display: 'none' }}>
-                                Estado
-                            </MenuItem>
-                            <MenuItem value="En espera">En espera</MenuItem>
-                            <MenuItem value="Entregado">Entregado</MenuItem>
-                            <MenuItem value="Cancelado">Cancelado</MenuItem>
-                        </Select>
-                    </ThemeProvider>
-                </div>
-                <Button variant="contained" disableRipple color='primary'>
+        <Grid container spacing={2} alignItems="center" style={{paddingBottom: 8}}>
+          <ThemeProvider theme={theme}>
+            <Grid item xs={12} md={1.5}>
+                <TextField
+                    size='small'
+                    placeholder="#ID"
+                    fullWidth
+                    style={{margin: 0, marginTop: matches ? 0 : 18}}
+                />
+            </Grid>
+            </ThemeProvider>
+            <ThemeProvider theme={theme}>
+            <Grid item xs={12} md={2}>
+                <Select
+                    value={orderBy}
+                    size='small'
+                    onChange={handleOrderByChange}
+                    fullWidth
+                    displayEmpty
+                    style={{color:"#757575"}}
+>
+                    <MenuItem value="" style={{ display: 'none' }}>Fecha</MenuItem>
+                    <MenuItem value="Ascendente">Ascendente</MenuItem>
+                    <MenuItem value="Descendente">Descendente</MenuItem>
+                </Select>
+            </Grid>
+            <Grid item xs={12} md={2}>
+                <Select
+                    size='small'
+                    value={filterByValue}
+                    onChange={handleFilterByValueChange}
+                    fullWidth
+                    displayEmpty
+                    style={{color:"#757575"}}
+>
+                    <MenuItem value="" style={{ display: 'none' }}>Valor</MenuItem>
+                    <MenuItem value="$100-$1000">$100 a $1000</MenuItem>
+                    <MenuItem value="$1000-$10000">$1k a $10k</MenuItem>
+                    <MenuItem value="$10000-$100000">$10k a $100k</MenuItem>
+                </Select>
+            </Grid>
+            <Grid item xs={12} md={2}>
+                <Select
+                    size='small'
+                    value={filterByStatus}
+                    onChange={handleFilterByStatusChange}
+                    fullWidth
+                    displayEmpty
+                    style={{color:"#757575"}}
+>
+                    <MenuItem value="" style={{ display: 'none' }}>Estado</MenuItem>
+                    <MenuItem value="En espera">En espera</MenuItem>
+                    <MenuItem value="Entregado">Entregado</MenuItem>
+                    <MenuItem value="Cancelado">Cancelado</MenuItem>
+                </Select>
+            </Grid>
+            </ThemeProvider>
+            <Grid item xs={12} md={4.5} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <Button variant="contained" disableRipple color='primary' style={{marginTop: matches ? 0 : 10}}>
                     Añadir
                 </Button>
-            </div>
-        </>
+            </Grid>
+        </Grid>
     );
 }
 
