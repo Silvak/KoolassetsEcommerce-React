5e936fcd26e6df961c5556519f2ded760d0cd649
francorinque <francoagustinrinquecorreo@gmail.com>
2024-03-13 13:05:50 -0300

    fix(dashboard): fix styles in summary

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,33 +1,30 @@
-import { AppBar, useMediaQuery } from "@mui/material";
+import { AppBar, Box, useMediaQuery } from "@mui/material";
 //components
-import { useEffect } from "react";
 import NavMobile from "@/components/navBar/NavMobile";
 import NavDesktop from "@/components/navBar/NavDesktop";
-import { useLocation } from "react-router-dom";
 
 const NavBar = () => {
   const isMobile = useMediaQuery((theme) => theme.breakpoints.down("lg"));
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
 
   return (
-    <AppBar
-      sx={{
-        background: "none",
-        boxShadow: "none",
-        padding: "20px 0",
-        color: "gray",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "flex-start",
-      }}
-      position="static"
-    >
-      {isMobile ? <NavMobile /> : <NavDesktop />}
-    </AppBar>
+    <Box component="header" sx={{ background: "inherit" }}>
+      <AppBar
+        sx={{
+          background: "none",
+          boxShadow: "none",
+          padding: "20px 0",
+          color: "gray",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          maxWidth: "1400px",
+          marginInline: "auto",
+        }}
+        position="static"
+      >
+        {isMobile ? <NavMobile /> : <NavDesktop />}
+      </AppBar>
+    </Box>
   );
 };
 export default NavBar;
