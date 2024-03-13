8c0a8a0dc2c3ccb9b2f4a2cb82089729512994af
francorinque <francoagustinrinquecorreo@gmail.com>
2024-03-13 20:13:12 -0300

    feat(NavMobileMenu): create only sidebar in mobile screen

--- src/components/navBar/NavMobileMenu.jsx
+++ src/components/navBar/NavMobileMenu.jsx
@@ -1,39 +1,97 @@
-import { ListItemButton, MenuList } from "@mui/material";
-import { NavLink } from "react-router-dom";
-import NavIcons from "@/components/navBar/NavIcons";
 import { useBoundStore } from "@/stores/index";
-import { menuItems } from "../../utils/menuItems";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+} from "@mui/material";
+import { NavLink, useLocation } from "react-router-dom";
+import { MENU_ITEMS, MENU_ITEMS_ADMIN } from "@/utils/menuItems";
+
+import { NavMobileMenuStyles } from "./styles";
 
 const NavMobileMenu = () => {
-  const { Authenticated } = useBoundStore((state) => state);
+  const { User } = useBoundStore((state) => state);
+  const css = NavMobileMenuStyles();
 
   return (
-    <MenuList sx={{ color: "text.primary", padding: "20px 10px" }}>
-      {menuItems.map((item, index) => {
-        if (index === 3) {
-          return <hr style={{ opacity: "10%" }} />;
-        }
-        return (
-          <ListItemButton key={item.id}>
-            <NavLink
-              to={item.to}
-              style={{ color: "inherit", textDecoration: "none" }}
+    <Box className={css.container}>
+      {MENU_ITEMS.map((item) => (
+        <Accordion className={css.accordion} key={item.id}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+            className={css.accordionSummary}
+          >
+            <h3 className={css.accordionTitle}>{item.title}</h3>
+          </AccordionSummary>
+          <AccordionDetails className={css.accordionDetails}>
+            {item.links.map((link) => (
+              <NavLink
+                key={link.id}
+                to={link.to}
+                className={({ isActive }) =>
+                  isActive ? css.linkActive : css.linkInactive
+                }
+              >
+                {link.icon && <link.icon />}
+                {link.label}
+              </NavLink>
+            ))}
+          </AccordionDetails>
+        </Accordion>
+      ))}
+      {User.isAdmin &&
+        MENU_ITEMS_ADMIN.map((item) => (
+          <Accordion className={css.accordion} key={item.id}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+              className={css.accordionSummary}
             >
-              {item.name}
-            </NavLink>
-          </ListItemButton>
-        );
-      })}
-
-      <hr style={{ opacity: "10%" }} />
-      <NavIcons
-        sx={{
-          justifyContent: "flex-start",
-          gap: "20px",
-          paddingLeft: "20px",
-        }}
-      />
-    </MenuList>
+              <h3 className={css.accordionTitle}>{item.title}</h3>
+            </AccordionSummary>
+            <AccordionDetails className={css.accordionDetails}>
+              {item.links.map((link) => (
+                <NavLink to={link.to} className={css.linkWrapper}>
+                  {link.icon && <link.icon />}
+                  {link.label}
+                </NavLink>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        ))}
+    </Box>
   );
 };
 export default NavMobileMenu;
+
+// <MenuList sx={{ color: "text.primary", padding: "20px 10px" }}>
+//   {menuItems.map((item, index) => {
+//     if (index === 3) {
+//       return <hr style={{ opacity: "10%" }} />;
+//     }
+//     return (
+//       <ListItemButton key={item.id}>
+//         <NavLink
+//           to={item.to}
+//           style={{ color: "inherit", textDecoration: "none" }}
+//         >
+//           {item.name}
+//         </NavLink>
+//       </ListItemButton>
+//     );
+//   })}
+
+//   <hr style={{ opacity: "10%" }} />
+//   <NavIcons
+//     sx={{
+//       justifyContent: "flex-start",
+//       gap: "20px",
+//       paddingLeft: "20px",
+//     }}
+//   />
+// </MenuList>
---
+++ src/components/navBar/styles.js
@@ -0,0 +1,45 @@
+import { makeStyles } from "@material-ui/core";
+
+export const NavMobileMenuStyles = makeStyles({
+  container: {
+    padding: "10px 20px",
+  },
+  accordion: {
+    background: "none",
+    boxShadow: "none",
+  },
+  accordionSummary: {
+    "& .css-bzs7dh-MuiAccordionSummary-expandIconWrapper": {
+      color: "#000",
+    },
+  },
+  accordionTitle: {
+    fontWeight: "bold",
+    fontSize: "18px",
+    color: "#000",
+    marginRight: "5px",
+  },
+  accordionDetails: {
+    fontSize: "16px",
+    fontWeight: "normal",
+    display: "flex",
+    flexDirection: "column",
+    gap: "10px",
+    paddingLeft: "30px",
+  },
+
+  linkInactive: {
+    textDecoration: "none",
+    gap: "8px",
+    display: "flex",
+    alignItems: "center",
+    color: "#00000080",
+  },
+  linkActive: {
+    textDecoration: "none",
+    gap: "8px",
+    display: "flex",
+    alignItems: "center",
+    color: "blue",
+  },
+});
