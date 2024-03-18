9943ddc1e335c14bf5653853a3fe224a1db584ca
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-03-18 12:07:52 -0300

    feat(product-orders): resize container for improved view

--- src/components/ProductOrder/ProductAdminMain.jsx
+++ src/components/ProductOrder/ProductAdminMain.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Breadcrumb from '../Breadcrumb/Breadcrumb'
 import ProductAdminTitle from './ProductAdminTitle'
-import ProductAdminSubtitle from '../ProductAdmin/ProductAdminSubtitle'
+import ProductAdminSubtitle from './ProductAdminSubtitle'
 import ProductAdminBar from '../ProductAdmin/ProductAdminBar'
 import ProductAdminContent from './ProductAdminContent'
 import { useMediaQuery } from '@mui/material'
@@ -9,11 +9,9 @@ import { useMediaQuery } from '@mui/material'
 function ProductAdminMain() {
 
   const isMobile = useMediaQuery('(max-width:600px)'); 
-  
   return (
     <div style={{ display: "flex", 
-    paddingRight: isMobile ? 0 : "5vw", 
-    flexDirection: "column", flex: 1, marginTop: isMobile ? "15%" : "5%", background:"none"}}>
+    flexDirection: "column", flex: 1, marginTop: isMobile ? "15%" : "5%"}}>
       <ProductAdminTitle />
       <Breadcrumb path={"Productos / Órdenes"} dashboard={"true"} />
       <ProductAdminSubtitle />
